b0546ef888f7db54d6edc875cabbeb2622514fa9
Aviwe Mbolekwa <mbolekwaaviwe@gmail.com>
2023-05-18 23:40:25 +0200

    Add files via upload

---
+++ assets/js/api.js
@@ -0,0 +1,28 @@
+/**
+ * @license MIT
+ * @author Aviwe Mbolekwa <mbolekwaaviwe@gmail.com>
+ * @copyright Aviwe Mbolekwa 2023
+ */
+
+'use strict';
+
+/**
+ * Fetch data from server
+ * @param {*} url API Url [required]
+ * @param {*} successCallback Success callback [required]
+ * @param {*} errorCallback Error callback [optional]
+ */
+
+export async function fetchData(url, successCallback, errorCallback) {
+
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  } else {
+    const error = await response.json();
+    errorCallback && errorCallback(error);
+  }
+
+}
